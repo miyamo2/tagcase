eee7f101be504566980d17ea46af47b213e3f43a
miyamo2 <miyamo2@outlook.com>
2025-07-05 22:45:47 +0900

    chore: minor correct in examples

--- .examples/target.go
+++ .examples/target.go
@@ -1,12 +1,12 @@
 package main
 
 type Foo struct {
-	SnakeAttribute          string `snake:"snake_attribute"`
-	CamelAttribute          string `camel:"camelAttribute"`
-	PascalAttribute         string `pascal:"PascalAttribute"`
-	KebabAttribute          string `kebab:"kebab-attribute"`
-	ScreamingSnakeAttribute string `screamingSnake:"SCREAMING_SNAKE_ATTRIBUTE"`
-	ScreamingKebabAttribute string `screamingKebab:"SCREAMING-KEBAB-ATTRIBUTE"`
+	SnakeAttribute          string `snake:"snake_Attribute"`
+	CamelAttribute          string `camel:"CamelAttribute"`
+	PascalAttribute         string `pascal:"pascalAttribute"`
+	KebabAttribute          string `kebab:"kebab_attribute"`
+	ScreamingSnakeAttribute string `screamingSnake:"ScreamingSnakeAttribute"`
+	ScreamingKebabAttribute string `screamingKebab:"ScreamingKebabAttribute"`
 	IgnoreAttribute         string `tagcase:"ignore" snake:"ignoreAttribute"`
 	InitialismAttribute     string `camel:"somethingID"`
 	MultiTagAttribute       string `snake:"multi_tag_attribute" camel:"multiTagAttribute" pascal:"MultiTagAttribute"`
